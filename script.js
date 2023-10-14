9d6f8512a46703ac0418a1be1fa4f152302bb95d
Ecorous <ecorous@pridecraft.gay>
2023-10-14 12:03:44 +0100

    site changes

--- script.js
+++ script.js
@@ -13,13 +13,20 @@ const hiddenElements = document.querySelectorAll(".reason");
 hiddenElements.forEach((el) => observer.observe(el));
 
 const rootElem = document.documentElement;
+const githubIconElems = document.getElementsByClassName("github-icon")
 
 const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
 if (darkThemeMq.matches) {
   rootElem.setAttribute("data-theme", "dark");
+  for (let i = 0; i < githubIconElems.length; i++) {
+    githubIconElems.item(i).setAttribute("data-theme",  "dark")
+  }
   document.getElementById("switcher").src = "images/sun.svg";
 } else {
   rootElem.setAttribute("data-theme", "light");
+  for (let x = 0; x < githubIconElems.length; x++) {
+    githubIconElems.item(x).setAttribute("data-theme",  "light")
+  }
   document.getElementById("switcher").src = "images/moon.svg";
 }
 
@@ -29,6 +36,9 @@ const switchTheme = () => {
   newTheme = dataTheme === "dark" ? "light" : "dark";
   switchIcon = document.getElementById("switcher");
   rootElem.setAttribute("data-theme", newTheme);
+  for (let x = 0; x < githubIconElems.length; x++) {
+    githubIconElems.item(x).setAttribute("data-theme",  newTheme)
+  }
   if (dataTheme === "dark") {
     switcher.src = "images/moon.svg";
   } else {
@@ -37,3 +47,4 @@ const switchTheme = () => {
 };
 
 document.querySelector("#switcher").addEventListener("click", switchTheme);
+feather.replace();
